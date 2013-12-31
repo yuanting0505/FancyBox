e88cdc7cbf841c865d224f03c80bc3690c498dcd
yuanting0505 <yuantingisme@gmail.com>
2013-12-31 11:25:03 +0800

    fancybox finished

---
+++ app/assets/javascript/fancybox.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".fancybox-thumb").fancybox({
+        prevEffect	: 'none',
+        nextEffect	: 'none',
+        helpers	: {
+            title	: {
+                type: 'outside'
+            },
+            thumbs	: {
+                width	: 50,
+                height	: 50
+            }
+        }
+    });
+});/**
+ * Created by OT on 13-12-31.
+ */
